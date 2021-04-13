e56b0c8cd9ec4816a9687733147355ee816e707a
OlesiaSub <lesya.sub@mail.ru>
2021-04-13 20:03:00 +0500

    Add time and space usage stat

--- app/src/main/java/ru/hse/coderank/analysis/main/Main.java
+++ app/src/main/java/ru/hse/coderank/analysis/main/Main.java
@@ -18,6 +18,7 @@ import ru.hse.coderank.analysis.pagerank.PageGraph;
 // temporary argument: "/home/olesya/HSE_2020-1/java/maze/out/artifacts/maze_jar/maze.jar"
 // "/home/olesya/github_scala/scala/out/artifacts/scala_jar2/scala.jar"
 // "/home/olesya/Downloads/junit-4.13.2.jar"
+// "/home/olesya/HSE_2020-1/JARsmth/scala-library-2.12.13.jar"
 
 public class Main {
 
@@ -33,7 +34,7 @@ public class Main {
         while (entries.hasMoreElements()) {
             JarEntry entry = entries.nextElement();
             String name = entry.getName();
-            System.out.println(name);
+//            System.out.println(name);
             if (name.endsWith(".class") && Configuration.processPackage(name)) {
                 try (InputStream stream = new BufferedInputStream(jarFile.getInputStream(entry), 1024)) {
                     ClassReader re = new ClassReader(stream);
@@ -44,24 +45,29 @@ public class Main {
         }
 
         graph.constructGraph();
-        for (Node<MethodNode> m : graph.storage) {
-            System.out.println("\nNEW METHOD");
-            System.out.println(m.payload.getName());
-            if (!graph.edges.get(m).isEmpty()) {
-                System.out.println("EDGES");
+//        for (Node<MethodNode> m : graph.storage) {
+//            System.out.println("\nNEW METHOD");
+//            System.out.println(m.payload.getName());
+//            if (!graph.edges.get(m).isEmpty()) {
+//                System.out.println("EDGES");
+//
+//                for (Node<MethodNode> me : graph.edges.get(m)) {
+//                    System.out.println(me.payload.getName());
+//                }
+//            }
+//        }
 
-                for (Node<MethodNode> me : graph.edges.get(m)) {
-                    System.out.println(me.payload.getName());
-                }
-            }
-        }
-
-        System.out.println("\nPAGERANK:");
+        System.out.print("\nSTARTING PAGERANK AT ");
+        System.out.println(System.currentTimeMillis() - time);
         PageGraph<MethodNode> pageGraph = new PageGraph<>(graph.storage, graph.edges, graph.parents);
         pageGraph.launchPageRank(50);
         pageGraph.getPageRank();
 
+        long usedBytes = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+        System.out.print("FINAL TIME: ");
         System.out.println(System.currentTimeMillis() - time);
+        System.out.print("FINAL SPACE: ");
+        System.out.println(usedBytes);
     }
 }
 
